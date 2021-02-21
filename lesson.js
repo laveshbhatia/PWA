1ec4e61c632b80bc92f79fa58c1a78b1102fed0e
laveshbhatia <lavesh2001@gmail.com>
2021-02-21 09:18:31 +0400

    Added new files

---
+++ lesson.js
@@ -0,0 +1,123 @@
+let lesson = [
+    {
+         "id": 101,
+                    "subject": "Math",
+                    "location": "London",
+                    "price": 30,
+                    "image": "images/math.jpg",
+                    "spaces": 5,
+                    "rating": 3,
+                    "quantity": 5
+},
+{
+         "id": 102,
+                    "subject": "English",
+                    "location": "Glasgow",
+                    "price": 30,
+                    "image": "images/english.jpg",
+                    "spaces": 5,
+                    "rating": 4,
+                    "quantity": 5
+},
+{
+         "id": 103,
+                    "subject": "Science",
+                    "location": "Edinburgh",
+                    "price": 50,
+                    "image": "images/science.jpg",
+                    "spaces": 5,
+                    "rating": 4,
+                    "quantity": 5
+},
+{
+         "id": 104,
+                    "subject": "IT",
+                    "location": "Bath",
+                    "price": 45,
+                    "image": "images/it.jpg",
+                    "spaces": 5,
+                    "rating": 5,
+                    "quantity": 5
+},
+{
+         "id": 105,
+                    "subject": "Chemistry",
+                    "location": "Belfast",
+                    "price": 42,
+                    "image": "images/chemistry.jpg",
+                    "spaces": 5,
+                    "rating": 3,
+                    "quantity": 5
+},
+{
+         "id": 106,
+                    "subject": "Math",
+                    "location": "Bath",
+                    "price": 40,
+                    "image": "images/math.jpg",
+                    "spaces": 5,
+                    "rating": 2,
+                    "quantity": 5
+},
+{
+         "id": 107,
+                    "subject": "Pyscology",
+                    "location": "Liverpool",
+                    "price": 60,
+                    "image": "images/pyscology.jpg",
+                    "spaces": 5,
+                    "rating": 1,
+                    "quantity": 5
+},
+{
+         "id": 108,
+                    "subject": "Physics",
+                    "location": "Cornwall",
+                    "price": 45,
+                    "image": "images/physics.jpg",
+                    "spaces": 5,
+                    "rating": 5,
+                    "quantity": 5
+},
+{
+         "id": 109,
+                    "subject": "History",
+                    "location": "York",
+                    "price": 40,
+                    "image": "images/history.jpg",
+                    "spaces": 5,
+                    "rating": 2,
+                    "quantity": 5
+},
+{
+         "id": 110,
+                    "subject": "Art",
+                    "location": "Oxford",
+                    "price": 10,
+                    "image": "images/art.jpg",
+                    "spaces": 5,
+                    "rating": 4,
+                    "quantity": 5
+},
+{
+         "id": 111,
+                    "subject": "Accounts",
+                    "location": "Oxford",
+                    "price": 80,
+                    "image": "images/accounts.jpg",
+                    "spaces": 5,
+                    "rating": 3,
+                    "quantity": 5
+},
+{
+         "id": 112,
+                    "subject": "Programming",
+                    "location": "Oxford",
+                    "price": 10,
+                    "image": "images/programming.jpg",
+                    "spaces": 5,
+                    "rating": 5,
+                    "quantity": 5
+}
+]
+    
